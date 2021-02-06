4d846f01ddea6df9063efeff1bcc8f709aeb9f16
Kayjay <cascadecustomer@Kayjays-Macbook.local>
2021-02-06 15:26:45 -0800

    Updates

---
+++ precache-manifest.f9a84b741e00e0d138cf7155976a390f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8c6f79e7aa971592546d109437052a1",
+    "url": "/Flashcard-Quiz/index.html"
+  },
+  {
+    "revision": "dc78e4633d27e9dbaf3f",
+    "url": "/Flashcard-Quiz/static/css/main.aab0f51b.chunk.css"
+  },
+  {
+    "revision": "b5158eec25c2841d29b1",
+    "url": "/Flashcard-Quiz/static/js/2.4cbe7585.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Flashcard-Quiz/static/js/2.4cbe7585.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc78e4633d27e9dbaf3f",
+    "url": "/Flashcard-Quiz/static/js/main.40a10c85.chunk.js"
+  },
+  {
+    "revision": "412dd26a216fecc56148",
+    "url": "/Flashcard-Quiz/static/js/runtime-main.240cea5d.js"
+  }
+]);
